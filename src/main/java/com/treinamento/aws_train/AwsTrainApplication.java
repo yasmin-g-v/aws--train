f5c130f650cd13d35a4e2b7ef827b06a60aa2adc
yasminvizeu <vizeugilyasmin@gmail.com>
2026-07-02 19:56:44 -0300

    primeiro commit

---
+++ src/main/java/com/treinamento/aws_train/AwsTrainApplication.java
@@ -0,0 +1,13 @@
+package com.treinamento.aws_train;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsTrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsTrainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/treinamento/aws_train/AwsTrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treinamento.aws_train;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsTrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
