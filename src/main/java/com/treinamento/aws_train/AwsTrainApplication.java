2ffa55ee119bdfacc21646dbdc209ea5a159cb77
yasminvizeu <vizeugilyasmin@gmail.com>
2026-07-02 20:26:18 -0300

    feat: add hello world api and tomcat

--- src/main/java/com/treinamento/aws_train/AwsTrainApplication.java
+++ src/main/java/com/treinamento/aws_train/AwsTrainApplication.java
@@ -2,12 +2,19 @@ package com.treinamento.aws_train;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController // Diz ao Spring que esta classe vai responder requisições HTTP
 public class AwsTrainApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AwsTrainApplication.class, args);
 	}
 
+	@GetMapping("/hello") // Define que o caminho para acessar será /hello
+	public String helloWorld() {
+		return "Hello World! Minha API no Spring Boot funcionou! 🚀";
+	}
 }
